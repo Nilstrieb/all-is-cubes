3bc14fc60cc4e497a7ebc949d1aba411ba260701
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-03-28 08:49:03 +0200

    add cargo-minimize scripts

---
+++ minimize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp premesh.rs all-is-cubes/src/mesh/chunked_mesh.rs
+cargo clean -p all-is-cubes
+cargo build -p all-is-cubes
+git apply crash.patch
+
+OUT=$(cargo build -p all-is-cubes 2>&1)
+
+if echo $OUT | grep "internal compiler error";
+then
+    echo "The ICE reproduces"
+    exit 0
+else
+    echo "No reproduction"
+    exit 1
+fi
