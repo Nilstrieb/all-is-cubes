432fb03b819eecb7c0cb7b6f52e83ce8b3a1a443
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 20:33:05 +0200

    more

--- all-is-cubes/src/mesh/chunked_mesh.rs
+++ all-is-cubes/src/mesh/chunked_mesh.rs
@@ -1,25 +1,15 @@
-use std::num::NonZeroU32;
-use std::sync::{Mutex, Weak};
-
-use fnv::{FnvHashMap, FnvHashSet};
-
-use crate::chunking::ChunkPos;
-use crate::listen::Listener;
-use crate::math::GridCoordinate;
-use crate::space::{BlockIndex, SpaceChange};
-
 /// If true, enables reporting chunk update timing at [`log::trace`] level.
 
 /// Stores a [`SpaceMesh`] covering one chunk of a [`Space`], caller-provided rendering
 /// data, and incidentals.
 #[derive(Debug, Eq, PartialEq)]
-pub struct ChunkMesh<const CHUNK_SIZE: GridCoordinate> {
+pub struct ChunkMesh<const CHUNK_SIZE: usize> {
     position: [(); CHUNK_SIZE],
 }
 
 /////// MOVE START
 
-impl<const CHUNK_SIZE: GridCoordinate> ChunkMesh<CHUNK_SIZE> {}
+impl<const CHUNK_SIZE: usize> ChunkMesh<CHUNK_SIZE> {}
 
 /// What might be dirty about a single chunk.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
