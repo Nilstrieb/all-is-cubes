462cd1587cf4bde4f2531fc87ead09ba48a13dde
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 21:05:06 +0200

    more

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,4 @@ pub struct ConstGeneric<const CHUNK_SIZE: usize> {
 
 /////// MOVE START
 impl<const CHUNK_SIZE: usize> ConstGeneric<CHUNK_SIZE> {}
-
-struct Empty {}
-
-impl core::fmt::Debug for Empty {
-    fn fmt(&self, _: &mut core::fmt::Formatter) -> core::fmt::Result {
-        loop {}
-    }
-}
 /////// MOVE END
