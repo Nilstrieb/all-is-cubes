d6cc612552033994cddb90104c71823574471f5e
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 20:15:28 +0200

    more

--- all-is-cubes/src/content.rs
+++
@@ -1,9 +0,0 @@
-//! Built-in “game content”: basic shapes and colors used in the UI and tests.
-//!
-//! This module is private; the public interface to this stuff is the separate
-//! `all-is-cubes-content` crate.
-use crate::block::{Block, Resolution};
-use crate::inv::Slot;
-use crate::math::{GridCoordinate, Rgba};
-use crate::space::{SetCubeError, Space};
-use crate::universe::Universe;
--- all-is-cubes/src/util/yield_progress.rs
+++
@@ -1,4 +0,0 @@
-use std::fmt;
-use std::panic::Location;
-use std::sync::Arc;
-use futures_core::future::{BoxFuture, Future};
