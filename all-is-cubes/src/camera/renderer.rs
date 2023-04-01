d8f810a2905758359af1dd83452fc2f8df73622b
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 20:17:05 +0200

    more

--- all-is-cubes/src/camera/renderer.rs
+++
@@ -1,5 +0,0 @@
-use embedded_graphics::mono_font::iso_8859_1::FONT_7X13_BOLD;
-use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::prelude::{PixelColor, Point};
-use embedded_graphics::text::{Baseline, Text};
-use embedded_graphics::Drawable;
