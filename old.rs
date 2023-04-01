4f75591238c9b1e2a885779eea8d590c47b91a82
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 20:59:41 +0200

    more

--- old.rs
+++ old.rs
@@ -5,6 +5,6 @@ pub struct ChunkMesh<const CHUNK_SIZE: usize> {
 
 /////// MOVE START
 impl<const CHUNK_SIZE: usize> ChunkMesh<CHUNK_SIZE> {}
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(Debug)]
 struct ChunkTodo {}
 /////// MOVE END
