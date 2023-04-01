91ac9de62169361b8c0acadc8ff134c209b086be
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 15:37:53 +0200

    more

--- all-is-cubes/src/camera.rs
+++ all-is-cubes/src/camera.rs
@@ -1,10 +1,10 @@
 //! Projection and view matrices, viewport and aspect ratio, visibility,
 //! raycasting into the scene, etc.
-use cgmath::{Basis3, Decomposed, Deg, Matrix4, Point2, Point3, Vector2, Vector3};
-use ordered_float::NotNan;
 use crate::chunking::OctantMask;
 use crate::math::{Aab, FreeCoordinate, GridAab, Rgba};
 use crate::raycast::Ray;
+use cgmath::{Basis3, Decomposed, Deg, Matrix4, Point2, Point3, Vector2, Vector3};
+use ordered_float::NotNan;
 mod flaws;
 pub(crate) use flaws::*;
 mod graphics_options;
@@ -16,253 +16,19 @@ type M = Matrix4<FreeCoordinate>;
 /// direction **to** the camera position in the world. This is done so that the
 /// [`Decomposed::disp`] vector is equal to the world position, rather than needing to
 /// be rotated by the view direction.
-pub(crate) type ViewTransform = Decomposed<
-    Vector3<FreeCoordinate>,
-    Basis3<FreeCoordinate>,
->;
+pub(crate) type ViewTransform = Decomposed<Vector3<FreeCoordinate>, Basis3<FreeCoordinate>>;
 /// Defines a viewpoint in/of the world: a viewport (aspect ratio), projection matrix,
 /// and view matrix.
 ///
 /// See also [`StandardCameras`], which adds self-updating from a character’s viewport,
 /// among other features.
 #[derive(Clone, Debug)]
-pub struct Camera {
-    /// Caller-provided options. Always validated by [`GraphicsOptions::repair`].
-    options: GraphicsOptions,
-    /// Caller-provided viewport.
-    viewport: Viewport,
-    /// Caller-provided view transform.
-    eye_to_world_transform: ViewTransform,
-    /// Inverse of `eye_to_world_transform` as a matrix.
-    /// Might also be called "view matrix".
-    /// Calculated by [`Self::compute_matrices`].
-    world_to_eye_matrix: M,
-    /// Projection matrix derived from viewport and options.
-    /// Calculated by [`Self::compute_matrices`].
-    projection: M,
-    /// View point derived from view matrix.
-    /// Calculated by [`Self::compute_matrices`].
-    view_position: Point3<FreeCoordinate>,
-    /// Inverse of `projection * world_to_eye_matrix`.
-    /// Calculated by [`Self::compute_matrices`].
-    inverse_projection_view: M,
-    /// Bounds of the visible area in world space.
-    /// Calculated by [`Self::compute_matrices`].
-    view_frustum: FrustumPoints,
-    /// Scale factor for scene brightness.
-    /// Calculated from `options.exposure` by [`Self::set_options`].
-    exposure_value: NotNan<f32>,
-}
-#[allow(clippy::cast_lossless)]
-impl Camera {
-    /// Create a camera which has
-    ///
-    /// * `options` and `viewport` as given,
-    /// * a `view_transform` of [`ViewTransform::one()`], and
-    /// * an `exposure` determined based on the graphics options.
-    pub(crate) fn new(options: GraphicsOptions, viewport: Viewport) -> Self {
-        loop {}
-    }
-    /// Returns the viewport value last provided.
-    pub(crate) fn viewport(&self) -> Viewport {
-        loop {}
-    }
-    /// Returns the [`GraphicsOptions`] value last provided (possibly with adjusted values).
-    pub(crate) fn options(&self) -> &GraphicsOptions {
-        loop {}
-    }
-    /// Replace the [`GraphicsOptions`] stored in this camera with
-    /// [`options.repair()`](GraphicsOptions::repair).
-    pub(crate) fn set_options(&mut self, options: GraphicsOptions) {
-        loop {}
-    }
-    /// Sets the contained viewport value, and recalculates matrices to be suitable for
-    /// the new viewport's aspect ratio.
-    pub(crate) fn set_viewport(&mut self, viewport: Viewport) {
-        loop {}
-    }
-    /// Returns the field of view, expressed in degrees on the vertical axis (that is, the
-    /// horizontal field of view depends on the viewport's aspect ratio).
-    /// This differs from the value in [`GraphicsOptions`] by being clamped to valid values.
-    pub(crate) fn fov_y(&self) -> Deg<FreeCoordinate> {
-        loop {}
-    }
-    /// Returns the view distance; the far plane of the projection matrix, or the distance
-    /// at which rendering may be truncated.
-    pub(crate) fn view_distance(&self) -> FreeCoordinate {
-        loop {}
-    }
-    /// Sets the view transform.
-    ///
-    /// Besides controlling rendering, this is used to determine world coordinates for purposes
-    /// of [`view_position`](Self::view_position) and
-    /// [`project_ndc_into_world`](Self::project_ndc_into_world).
-    ///
-    /// The scale currently must be 1.
-    #[track_caller]
-    #[allow(clippy::float_cmp)]
-    pub(crate) fn set_view_transform(&mut self, eye_to_world_transform: ViewTransform) {
-        loop {}
-    }
-    /// Gets the last eye-to-world transform set by [`Self::set_view_transform()`].
-    pub(crate) fn get_view_transform(&self) -> ViewTransform {
-        loop {}
-    }
-    /// Returns a projection matrix suitable for OpenGL use.
-    pub(crate) fn projection(&self) -> M {
-        loop {}
-    }
-    /// Returns a view matrix suitable for OpenGL use.
-    pub(crate) fn view_matrix(&self) -> M {
-        loop {}
-    }
-    /// Returns the eye position in world coordinates, as set by [`Camera::set_view_transform()`].
-    pub(crate) fn view_position(&self) -> Point3<FreeCoordinate> {
-        loop {}
-    }
-    /// Returns an [`OctantMask`] including all directions this camera's field of view includes.
-    pub(crate) fn view_direction_mask(&self) -> OctantMask {
-        loop {}
-    }
-    /// Converts a screen position in normalized device coordinates (as produced by
-    /// [`Viewport::normalize_nominal_point`]) into a ray in world space.
-    /// Uses the view transformation given by [`set_view_transform`](Self::set_view_transform).
-    pub(crate) fn project_ndc_into_world(&self, ndc: Point2<FreeCoordinate>) -> Ray {
-        loop {}
-    }
-    fn project_point_into_world(
-        &self,
-        p: Point3<FreeCoordinate>,
-    ) -> Point3<FreeCoordinate> {
-        loop {}
-    }
-    /// Determine whether the given `Aab` is visible in this projection+view.
-    pub(crate) fn aab_in_view(&self, aab: Aab) -> bool {
-        loop {}
-    }
-    /// Helper for [`aab_in_view`]; finds if two sets of points' projections onto a line intersect.
-    ///
-    /// Note: NOT `#[inline]` because profiling shows that to have a negative effect.
-    fn separated_along(
-        points1: impl IntoIterator<Item = Point3<FreeCoordinate>>,
-        points2: impl IntoIterator<Item = Point3<FreeCoordinate>>,
-        axis: Vector3<FreeCoordinate>,
-    ) -> bool {
-        loop {}
-    }
-    /// Apply postprocessing steps determined by this camera to convert a HDR “scene”
-    /// color into a LDR “image” color. Specifically:
-    ///
-    /// 1. Multiply the input by this camera's exposure value.
-    /// 2. Apply the tone mapping operator specified in [`Camera::options()`].
-    pub(crate) fn post_process_color(&self, color: Rgba) -> Rgba {
-        loop {}
-    }
-    /// Returns the current exposure value for scaling luminance.
-    ///
-    /// Renderers should use this value.
-    pub(crate) fn exposure(&self) -> NotNan<f32> {
-        loop {}
-    }
-    /// Set the exposure value determined by average scene brightness.
-    /// This may or may not affect [`Self::exposure()`] depending on the current
-    /// graphics options.
-    pub(crate) fn set_measured_exposure(&mut self, value: f32) {
-        loop {}
-    }
-    fn compute_matrices(&mut self) {
-        loop {}
-    }
-}
+pub struct Camera {}
 /// Viewport dimensions for rendering and UI layout with the correct resolution and
 /// aspect ratio.
 #[allow(clippy::exhaustive_structs)]
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub(crate) struct Viewport {
-    /// Viewport dimensions to use for determining aspect ratio and interpreting
-    /// pointer events.
-    pub(crate) nominal_size: Vector2<FreeCoordinate>,
-    /// Viewport dimensions to use for framebuffer configuration.
-    /// This aspect ratio may differ to represent non-square pixels.
-    pub(crate) framebuffer_size: Vector2<u32>,
-}
-impl Viewport {
-    #![allow(clippy::cast_lossless)]
-    /// Construct a Viewport from a pixel count and a scale factor.
-    ///
-    /// The `nominal_size` will be the given `framebuffer_size` divided by the given
-    /// `scale_factor`.
-    pub(crate) fn with_scale(scale_factor: f64, framebuffer_size: Vector2<u32>) -> Self {
-        loop {}
-    }
-    /// A meaningless but valid [`Viewport`] value for use in tests which require one
-    /// but do not care about its effects.
-    #[doc(hidden)]
-    pub(crate) const ARBITRARY: Viewport = Viewport {
-        nominal_size: Vector2::new(2.0, 2.0),
-        framebuffer_size: Vector2::new(2, 2),
-    };
-    /// Calculates the aspect ratio (width divided by height) of the `nominal_size` of this
-    /// viewport.
-    ///
-    /// If the result would naturally be infinite or undefined then it is reported as 1
-    /// instead. This is intended to aid in robust handling of degenerate viewports which
-    /// contain no pixels.
-    #[inline]
-    pub(crate) fn nominal_aspect_ratio(&self) -> FreeCoordinate {
-        loop {}
-    }
-    /// Convert an *x* coordinate from the range `0..self.framebuffer_size.x` (upper exclusive)
-    /// to OpenGL normalized device coordinates, range -1 to 1 (at pixel centers).
-    #[inline]
-    pub(crate) fn normalize_fb_x(&self, x: usize) -> FreeCoordinate {
-        loop {}
-    }
-    /// Convert a *y* coordinate from the range `0..self.framebuffer_size.y` (upper exclusive)
-    /// to OpenGL normalized device coordinates, range -1 to 1 (at pixel centers) and flipped.
-    #[inline]
-    pub(crate) fn normalize_fb_y(&self, y: usize) -> FreeCoordinate {
-        loop {}
-    }
-    /// Convert an *x* coordinate from the range `0..=self.framebuffer_size.x` (inclusive)
-    /// to OpenGL normalized device coordinates, range -1 to 1 (at pixel *edges*).
-    #[inline]
-    pub(crate) fn normalize_fb_x_edge(&self, x: usize) -> FreeCoordinate {
-        loop {}
-    }
-    /// Convert a *y* coordinate from the range `0..=self.framebuffer_size.y` (inclusive)
-    /// to OpenGL normalized device coordinates, range -1 to 1 (at pixel *edges*) and flipped.
-    #[inline]
-    pub(crate) fn normalize_fb_y_edge(&self, y: usize) -> FreeCoordinate {
-        loop {}
-    }
-    /// Convert a point in the [`Self::nominal_size`] coordinate system to
-    /// to OpenGL normalized device coordinates, range -1 to 1 (at pixel centers) with Y flipped.
-    ///
-    /// TODO: Some windowing APIs providing float input might have different ideas of pixel centers.
-    #[inline]
-    pub(crate) fn normalize_nominal_point(
-        &self,
-        nominal_point: Point2<f64>,
-    ) -> Vector2<FreeCoordinate> {
-        loop {}
-    }
-    /// Computes the number of pixels in the framebuffer.
-    /// Returns [`None`] if that number does not fit in a [`usize`].
-    pub(crate) fn pixel_count(&self) -> Option<usize> {
-        loop {}
-    }
-}
-#[cfg(feature = "arbitrary")]
-#[mutants::skip]
-impl<'a> arbitrary::Arbitrary<'a> for Viewport {
-    fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
-        loop {}
-    }
-    fn size_hint(depth: usize) -> (usize, Option<usize>) {
-        loop {}
-    }
-}
+pub(crate) struct Viewport {}
 /// Calculate an “eye position” (camera position) to view the entire given `bounds`.
 ///
 /// `direction` points in the direction the camera should be relative to the space.
@@ -278,37 +44,4 @@ pub(crate) fn eye_for_look_at(
 /// A view frustum, represented by its corner points.
 /// This is an underconstrained representation, but one that is useful to precompute.
 #[derive(Clone, Copy, Debug, PartialEq)]
-struct FrustumPoints {
-    lbf: Point3<FreeCoordinate>,
-    rbf: Point3<FreeCoordinate>,
-    ltf: Point3<FreeCoordinate>,
-    rtf: Point3<FreeCoordinate>,
-    lbn: Point3<FreeCoordinate>,
-    rbn: Point3<FreeCoordinate>,
-    ltn: Point3<FreeCoordinate>,
-    rtn: Point3<FreeCoordinate>,
-    bounds: Aab,
-}
-impl Default for FrustumPoints {
-    fn default() -> Self {
-        loop {}
-    }
-}
-impl FrustumPoints {
-    fn iter(self) -> impl Iterator<Item = Point3<FreeCoordinate>> {
-        [self.lbf, self.rbf, self.ltf, self.rtf, self.lbn, self.rbn, self.ltn, self.rtn]
-            .into_iter()
-    }
-    fn compute_bounds(&mut self) {
-        loop {}
-    }
-}
-/// Projects a set of points onto an axis and returns the least and greatest dot product
-/// with the axis vector.
-#[inline(always)]
-fn projected_range(
-    points: impl IntoIterator<Item = Point3<FreeCoordinate>>,
-    axis: Vector3<FreeCoordinate>,
-) -> (FreeCoordinate, FreeCoordinate) {
-    loop {}
-}
+struct FrustumPoints {}
