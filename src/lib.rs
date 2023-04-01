74eea4ea88c2c22135941bf6850df3fee0ef1951
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-01 21:01:50 +0200

    more

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 #[derive(Debug)]
 pub struct ChunkMesh<const CHUNK_SIZE: usize> {
     position: [(); CHUNK_SIZE],
@@ -5,6 +7,12 @@ pub struct ChunkMesh<const CHUNK_SIZE: usize> {
 
 /////// MOVE START
 impl<const CHUNK_SIZE: usize> ChunkMesh<CHUNK_SIZE> {}
-#[derive(Debug)]
 struct ChunkTodo {}
+
+#[automatically_derived]
+impl ::core::fmt::Debug for ChunkTodo {
+    fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+        ::core::fmt::Formatter::write_str(f, "ChunkTodo")
+    }
+}
 /////// MOVE END
